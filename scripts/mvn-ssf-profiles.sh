76c42e9663c998e93bc333a0f254a9d3619537bd
Maurizio Pillitu <maoo@apache.org>
2016-07-10 22:52:37 +0200

    Update mvn-ssf-profiles.sh
    allowing overwrites via env variables

--- scripts/mvn-ssf-profiles.sh
+++ scripts/mvn-ssf-profiles.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-set -ev
+set -e
 
-echo "[MVN-SSF] Using Branch ${TRAVIS_BRANCH}"
+if [ -z "$SCM_BRANCH" ]
+then
+  SCM_BRANCH=${TRAVIS_BRANCH}
+fi  
 
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-  MVN_PROFILES="sonar,versioneye"
+if [ -z "$MVN_COMMAND" ]
+then
+  MVN_COMMAND="mvn clean package"
+fi  
+
+if [ -z "$MVN_MASTER_PROFILES" ]
+then
+  MVN_MASTER_PROFILES="versioneye"
+fi  
+
+if [ -z "$MVN_ALLBRANCHES_PROFILES" ]
+then
+  MVN_ALLBRANCHES_PROFILES=""
+fi  
+
+echo "[MVN-SSF] Using Branch $SCM_BRANCH"
+
+if [ "$SCM_BRANCH" = "master" ]; then
+  MVN_PROFILES=$MVN_MASTER_PROFILES
 else
-  MVN_PROFILES="sonar"
+  MVN_PROFILES=$MVN_ALLBRANCHES_PROFILES
 fi
 
 echo "[MVN-SSF] Invoking mvn using profiles $MVN_PROFILES"
 
-mvn clean package -P$MVN_PROFILES
+$MVN_COMMAND -P$MVN_PROFILES
