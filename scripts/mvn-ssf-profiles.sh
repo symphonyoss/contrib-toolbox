9fa94924a5074de7d1b572cc055718e15fbf3588
Maurizio Pillitu <maoo@apache.org>
2016-07-10 22:44:41 +0200

    Update mvn-ssf-profiles.sh
    fixed mvn command

--- scripts/mvn-ssf-profiles.sh
+++ scripts/mvn-ssf-profiles.sh
@@ -5,11 +5,11 @@ set -ev
 echo "[MVN-SSF] Using Branch ${TRAVIS_BRANCH}"
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-	MVN_PROFILES="sonar,versioneye"
+  MVN_PROFILES="sonar,versioneye"
 else
   MVN_PROFILES="sonar"
 fi
 
 echo "[MVN-SSF] Invoking mvn using profiles $MVN_PROFILES"
 
-mvn clean package -P$MVN_PROFILES; else mvn clean package -Psonar
+mvn clean package -P$MVN_PROFILES
