74a7501a31bfe28a77cab4ed0c921f38288db94b
Maurizio Pillitu <maoo@apache.org>
2017-05-13 15:33:07 +0200

    --from-archive oc option doesnt exist
    Reverting last commit

--- scripts/oc-deploy.sh
+++ scripts/oc-deploy.sh
@@ -29,7 +29,7 @@
 
 # Environment variables needed:
 # - OC_TOKEN - The Openshift Online token
-# - OC_BINARY_FOLDER (or OC_BINARY_ARCHIVE) - contains the local path to the binary folder to upload to the container as source
+# - OC_BINARY_FOLDER - contains the local path to the binary folder to upload to the container as source
 # - OC_BUILD_CONFIG_NAME - the name of the BuildConfig registered in Openshift
 
 # Environment variables overrides:
@@ -41,8 +41,8 @@ if [[ -z "$OC_TOKEN" ]]; then
   echo "Missing OC_TOKEN. Failing."
   exit -1
 fi
-if [[ -z "$OC_BINARY_FOLDER" ] || [ -z "$OC_BINARY_ARCHIVE" ]]; then
-  echo "Missing OC_BINARY_FOLDER or OC_BINARY_ARCHIVE. Failing."
+if [[ -z "$OC_BINARY_FOLDER" ]]; then
+  echo "Missing OC_BINARY_FOLDER. Failing."
   exit -1
 fi
 if [[ -z "$OC_BUILD_CONFIG_NAME" ]]; then
@@ -73,8 +73,4 @@ echo "Logged into api.preview.openshift.com"
 if [[ -n "$OC_BINARY_FOLDER" ]]; then
   oc start-build $OC_BUILD_CONFIG_NAME --from-dir=$OC_BINARY_FOLDER --wait=true
   echo "Build of $OC_BUILD_CONFIG_NAME from folder $OC_BINARY_FOLDER completed"
-fi
-if [[ -n "$OC_BINARY_ARCHIVE" ]]; then
-  oc start-build $OC_BUILD_CONFIG_NAME --from-archive=$OC_BINARY_ARCHIVE --wait=true
-  echo "Build of $OC_BUILD_CONFIG_NAME from archive $OC_BINARY_ARCHIVE completed"
 fi
