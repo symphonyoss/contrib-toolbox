71718b26d8e67bd6555a62dcb2f14748f22f5d0e
Maurizio Pillitu <maoo@apache.org>
2017-08-29 14:49:09 +0200

    :ambulance: escape var values

--- scripts/oc-deploy.sh
+++ scripts/oc-deploy.sh
@@ -98,11 +98,7 @@ if [[ -n "$OC_TEMPLATE_PROCESS_ARGS" ]]; then
   do
     VAR_NAME=$i
     get_branch_var $i
-    # export PROCESS_ARGS="$PROCESS_ARGS -p ${i}=\"${VAR_VALUE}\""
-    if [[ -n "$BR_VAR" ]]; then
-      VAR_NAME=$BR_VAR
-    fi
-    export PROCESS_ARGS="$PROCESS_ARGS -p ${i}=\$${VAR_NAME}"
+    export PROCESS_ARGS="$PROCESS_ARGS -p ${i}=\"${VAR_VALUE}\""
   done
   echo "Process args is: ${PROCESS_ARGS}"
 fi
